12c243eeccd12fe2cce4e94fce3c57bdc2229379
Vanya123456 <ivan.buyukli21@gmail.com>
2021-02-17 21:34:25 +0100

    Delete HW_Lesson_1 directory

--- HW_Lesson_1/Task_1.py
+++
@@ -1,6 +0,0 @@
-duration = int(input('Enter seconds '))
-d = duration // 86400
-h = duration % 86400 // 3600
-m = duration % 3600 // 60
-s = duration % 3600 % 60
-print(f'{d} days {h} hours {m} minutes {s} seconds')
